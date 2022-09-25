e9ffe22f8d472db735cf21f7acf62b0268f57985
Matt Jackson <matt@jacksonsix.com>
2022-09-25 13:37:07 -0400

    cleaned up comments for domain model testing

--- examples/domainmodel/test/naming.test.ts
+++ examples/domainmodel/test/naming.test.ts
@@ -1,6 +1,7 @@
-/**
- * MIT License
- */
+/******************************************************************************
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
 
 import { AstNode, LangiumDocument, EmptyFileSystem, AstNodeDescription } from 'langium';
 import { parseDocument } from 'langium/test';
@@ -8,11 +9,17 @@ import { createDomainModelServices } from '../src/language-server/domain-model-m
 
 const services = createDomainModelServices(EmptyFileSystem).domainmodel;
 
-describe('Small Java Index: Qualified Names', () => {
+describe('Domain Model Scope Computation', () => {
 
     let testDoc: LangiumDocument<AstNode>;
     let exports: AstNodeDescription[];
+    let computedNames: string;
+    const expectedNames = 'E1, foo.bar.Complex, foo.bar.E2, baz.E3, baz.E4, baz.nested.E5';
 
+    /**
+     *  Note: `DomainModelScopeComputation` exports only types (`DataType or `Entity`) with
+     *  their qualified names.
+     */
     beforeAll(async () => {
         const text=`
         entity E1 {
@@ -49,10 +56,10 @@ describe('Small Java Index: Qualified Names', () => {
 
         testDoc = await parseDocument(services, text);
         exports = await services.references.ScopeComputation.computeExports(testDoc);
+        computedNames = exports.map(e => e.name).join(', ');
     });
 
-    it('Trying to get FQN Info', () => {
-        console.log(exports);
-        expect('A').toBe('C');
+    it('Exports Qualified Names', () => {
+        expect(computedNames).toBe(expectedNames);
     });
 });
